c0d9da6b7bc26219b12af4c919aa5507c8489b42
SanctusMarko <fortalezaosijek@gmail.com>
2024-06-27 22:18:20 +0200

    NachLoschenTaskApp2706

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
@@ -2,8 +2,22 @@ import 'package:flutter/material.dart';
 
 class S4522 extends StatelessWidget {
   const S4522({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        const Text(
+          "Hello Marko",
+          style: TextStyle(fontSize: 24),
+        ),
+        const SizedBox(height: 20),
+        ElevatedButton(
+          onPressed: () {},
+          child: const Text("Drück mich"),
+        ),
+      ],
+    );
   }
 }
--- lib/tasks/task_list.dart
+++ lib/tasks/task_list.dart
@@ -40,23 +40,23 @@ const List<Task> chapter3TaskList = [
 const List<Task> chapter4TaskList = [
   Task(
     taskModel: t_4_5_1_1,
-    //solution: S4511(),
+    solution: S4511(),
   ),
   Task(
     taskModel: t_4_5_2_1,
-    //solution: S4521(),
+    solution: S4521(),
   ),
   Task(
     taskModel: t_4_5_2_2,
-    // solution: S4522(),
+    solution: S4522(),
   ),
   Task(
     taskModel: t_4_5_2_3,
-    // solution: S4523(),
+    solution: S4523(),
   ),
   Task(
     taskModel: t_4_5_2_4,
-    // solution: S4524(),
+    solution: S4524(),
   ),
   Task(
     taskModel: t_4_5_3_1,
